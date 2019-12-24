98f3cde9a20be98126ddba61f1a75771cb88a50f
arman gasparyan <arman.gasparyan5.y@tumo.org>
2019-12-24 19:51:43 +0400

    stexcel about ej

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,55 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Travel-site Logo"
+          class="shrink mr-2"
+          contain
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Travel-site Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://www.freelogodesign.org/file/app/client/thumb/b30565f8-c1c0-41c2-b5ba-31c4c53f3311_200x200.png?1577196404599.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: 'App',
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,35 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 style="margin-left:800px;"> Travel-site travel Agensy</h1>
+    <v-parallax src="https://www.tsarvoyages-caucase.com/images/destinations/1918/dreamstime%20(c)%20monast%C3%A8re.jpg"></v-parallax>
+    <div style="margin-left:50px;">
+     <p ><b>Travel-site</b>- a touroperator that offers a wide range of tours and activities in Armenia,
+          from classic sightseeing excursions to adventure and special tours tailored to your needs and interests.
+            Our tour programs cover entire Armenia from north to south, offering travelers an opportunity to not just touch the surface of the country, 
+            but to experience Armenia as it is. We want to show you Armenia the way we see and feel it!</p>
+
+    <p>For us, Armenia isn't what one can see on glossy postcards. Armenia is a hidden treasure, 
+      it's in every rock and tree, river and lake, house and street, village and town, in every glass of wine raised to friendship and in every smile on people's faces.</p>
+
+    <p>Armenia is small in size, it occupies an area of about 30000 square kilometers. And yet Armenia is very diverse. 
+      The country is different in the north, south and center. The north, with its lush green forests and serene landscapes is a place to soothe your mind and soul.
+      Its meditative tranquility boosts the creative force within. The south of Armenia, with its ancient historical and spiritual sites , symbolizes our unbroken spirit. 
+        The center of Armenia, a link between the north and south, is the heart of the country, the source of origin of Armenian art and culture.</p>
+
+    <p>Explore Armenia following our unique routes and discover what fascinates us most about this land, visit sites of sacred energy, ancient, 
+      powerful and all-embracing, feel the spirit of Armenia,  absorb Armenia with each new experience, connect with people and make new friends. 
+      We are OverLand, and we invite you to join us on a mesmerizing journey around Armenia!</p>
+      </div>
+
   </div>
+  
 </template>
+
+<script>
+export default {
+  name: 'About',
+
+  data: () => ({
+  })
+};
+</script>
